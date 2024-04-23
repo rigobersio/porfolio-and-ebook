66ce99ebf7d418172b4b4463d04ec64928d5a35a
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-04-23 01:04:06 -0400

    (rizen)(Libro, home, NavBar, footer)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router-dom"
 
 import NavBar from "./component/navBar/navBar"
 import { RenderEbook } from "./component/libro/renderEbook"
+import Home from "./component/home/home"
+import Footer from "./component/footer/footer"
+
+
 
 function App() {
 
@@ -15,6 +19,8 @@ function App() {
           <Route path="/book" element={<RenderEbook />} />
           <Route path="/contact" />
         </Routes>
+        <Home />
+        <Footer />
       </div>
     </>
   )
--- front/src/component/navBar/navBar.tsx
+++ front/src/component/navBar/navBar.tsx
@@ -1,36 +1,16 @@
-//import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import "./navBar.css"
 
-const NavBar = () => {
+const Navbar: React.FC = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-              Inicio
-            </Link>
-          </Typography>
-          <Button color="inherit">
-            <Link to="/about" style={{ textDecoration: 'none', color: 'white' }}>
-              Sobre mí
-            </Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/book" style={{ textDecoration: 'none', color: 'white' }}>
-              Lee mi Libro
-            </Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/contact" style={{ textDecoration: 'none', color: 'white' }}>
-              Contacto
-            </Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <nav className="navbar">
+      <Link to="/">Inicio</Link>
+      <Link to="/about">Sobre mí</Link>
+      <Link to="/book">Lee mi Libro</Link>
+      <Link to="/contact">Contacto</Link>
+    </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
