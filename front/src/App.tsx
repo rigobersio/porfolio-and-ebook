ca7883d2e834b430ab5d56a21146e9fd5e9dee2c
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-04-23 23:24:54 -0400

    (rizen)new:Nav(Nav responsiva con varios errores para analizar)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,33 @@
 import { Routes, Route } from "react-router-dom"
 
-import NavBar from "./component/navBar/navBar"
 import { RenderEbook } from "./component/libro/renderEbook"
 import Home from "./component/home/home"
 import Footer from "./component/footer/footer"
+import Nav from "./components/nav/Nav"
 
 
 
 function App() {
-
+  
   return (
     <>
-      <div>
-        <NavBar />
-        <Routes>
+    <div className="bg-stale-900">
+      <Nav />
+    </div>
+    </>
+  )
+}
+
+export default App
+
+/*
+ <Routes>
           <Route path="/" />
           <Route path="/about" />
           <Route path="/book" element={<RenderEbook />} />
           <Route path="/contact" />
         </Routes>
         <Home />
-        <Footer />
-      </div>
-    </>
-  )
-}
 
-export default App
+
+*/
