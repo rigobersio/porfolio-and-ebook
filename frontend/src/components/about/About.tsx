775bcd532ba44ce5a51a671d5f718b162c993e02
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-07 23:31:53 -0400

    (ryzen)edit:About(control de efectos tailwind con react-intersection-observer)

--- frontend/src/components/about/About.tsx
+++ frontend/src/components/about/About.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaGithub, FaWhatsapp } from "react-icons/fa";
 import { ImLinkedin } from "react-icons/im";
+import { useInView } from "react-intersection-observer";
 
 const About: React.FC = () => {
+  const [isIntroVisible, setIsIntroVisible] = useState(false);
+  const [isAboutMeVisible, setIsAboutMeVisible] = useState(false);
+  const { ref: introRef, inView: isIntroInView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+  const { ref: aboutMeRef, inView: isAboutMeInView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
 
-    return (
-      <div className="text-center pt-10 pb-20 lg:w-full">
-       <div className="relative top-10">
-       <h2 className=" text-2xl">console.log(<strong>"Hello World"</strong>);</h2>
+  useEffect(() => {
+    setIsIntroVisible(isIntroInView);
+    setIsAboutMeVisible(isAboutMeInView);
+  }, [isIntroInView, isAboutMeInView]);
+
+  return (
+    <div className="text-center pt-10 pb-20 lg:w-full">
+      <div className="relative top-10">
+        <h2 className=" text-2xl">console.log(<strong>"Hello World"</strong>);</h2>
         <h2 className="text-3xl my-2">My name is <strong>Rigoberto</strong></h2>
-        <p className="w-3/4 mx-auto">I am a web developer, que no sabe demasiado ingles, por lo tanto, te invito a descargar mi CV en castellano.</p>
+        {isIntroVisible && (
+          <p className="w-3/4 mx-auto">I am a web developer, que no sabe demasiado ingles, por lo tanto, te invito a descargar mi CV en castellano.</p>
+        )}
         <button className="gap-2 px-[20px] py-[5px] rounded mt-6 bg-[#1484d6] border border-[#0e0e0f] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6]">Descarga mi CV</button>
         <div>
           <div className="flex gap-10 justify-center mt-5">
@@ -26,36 +44,37 @@ const About: React.FC = () => {
               <FaWhatsapp className='w-7 h-7 rounded' />
             </a>
           </div>
-            <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-              <img className="object-cover object-center rounded-full h-96 w-96" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1714359052/codigo/equipoViejo_yj3ibg.jpg" alt="computadora vieja con teclado" />
-            </div>
+          <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+            <img className="object-cover object-center rounded-full h-96 w-96" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1714359052/codigo/equipoViejo_yj3ibg.jpg" alt="computadora vieja con teclado" />
+          </div>
         </div>
-       </div>
-       <div id="SobreMi">
-       <h2 className="p-[3rem] text-3xl my-2 hover:text-purple-800 transition"><strong>Sobre mí</ strong></h2>
-       <div className="flex justify-around flex-col text-justify lg:flex-row">
-        <div className="flex items-center justify-center lg:w-1/3">
-          <img className="object-cover object-center rounded-full h-60 w-60 animate-pulse animate-once animate-duration-[3500ms]" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1715016763/myPerfil/1710771555673_Git-Hub_unujoi.jpg" alt="Imagen del programador" />
-        </div>
-        <div className="lg:w-2/3">
-        <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[3000ms]">
-       Soy un apasionado del desarrollo web con un trasfondo en ciencias agronómicas que encontró su verdadera pasión en la vanguardia tecnológica. 💻
-       </p>
-       <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[3500ms]">
-Como Full Stack Web Developer Junior, me he decantado por tecnologías clave como <strong>JavaScript, TypeScript, React, Redux, Node, Express, PostgreSQL, Mongo y Git</ strong>. Estas herramientas me han permitido crear aplicaciones web funcionales y atractivas. Al mismo tiempo, mi experiencia general en TI ocupando diferentes plataformas webs, programas de escritorio y sistemas operativos me ayudan a ampliar las posibilidades a la hora de solucionar problemas. 💼💻
-       </p>
-       <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[4000ms]">
-Mi experiencia en el programa de formación en desarrollo web en "Henry" me ha proporcionado una base sólida en la programación y la resolución de problemas, mientras que mi formación en ciencias agronómicas en la Universidad de Concepción me ha dotado de un enfoque analítico y orientado a soluciones. 🔍💡
-       </p>
-       <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[4500ms]">
-Estoy en busca de nuevas oportunidades para aplicar mis habilidades y seguir creciendo profesionalmente, estoy emocionado por la posibilidad de colaborar en proyectos innovadores o formar parte de un equipo que comparta mi pasión por la tecnología. 🚀
-       </p>
-        </div>
-       </div>
-       
-       </div>
       </div>
-    );
-}
+      <div id="SobreMi" ref={aboutMeRef}>
+        <h2 className="p-[3rem] text-3xl my-2 hover:text-purple-800 transition"><strong>Sobre mí</strong></h2>
+        {isAboutMeVisible && (
+          <div className="flex justify-around flex-col text-justify lg:flex-row">
+            <div className="flex items-center justify-center lg:w-1/3">
+              <img className="object-cover object-center rounded-full h-60 w-60 animate-pulse animate-once animate-duration-[3500ms]" src="https://res.cloudinary.com/dqh2illb5/image/upload/v1715016763/myPerfil/1710771555673_Git-Hub_unujoi.jpg" alt="Imagen del programador" />
+            </div>
+            <div className="lg:w-2/3">
+              <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[3000ms]">
+                Soy un apasionado del desarrollo web con un trasfondo en ciencias agronómicas que encontró su verdadera pasión en la vanguardia tecnológica. 💻
+              </p>
+              <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[3500ms]">
+                Como Full Stack Web Developer Junior, me he decantado por tecnologías clave como <strong>JavaScript, TypeScript, React, Redux, Node, Express, PostgreSQL, Mongo y Git</strong>. Estas herramientas me han permitido crear aplicaciones web funcionales y atractivas. Al mismo tiempo, mi experiencia general en TI ocupando diferentes plataformas webs, programas de escritorio y sistemas operativos me ayudan a ampliar las posibilidades a la hora de solucionar problemas. 💼💻
+              </p>
+              <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[4000ms]">
+                Mi experiencia en el programa de formación en desarrollo web en "Henry" me ha proporcionado una base sólida en la programación y la resolución de problemas, mientras que mi formación en ciencias agronómicas en la Universidad de Concepción me ha dotado de un enfoque analítico y orientado a soluciones. 🔍💡
+              </p>
+              <p className="pt-6 w-3/4 mx-auto animate-fade animate-once animate-duration-[4500ms]">
+                Estoy en busca de nuevas oportunidades para aplicar mis habilidades y seguir creciendo profesionalmente, estoy emocionado por la posibilidad de colaborar en proyectos innovadores o formar parte de un equipo que comparta mi pasión por la tecnología. 🚀
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default About;
