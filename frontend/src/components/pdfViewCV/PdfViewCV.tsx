6d7df87ccd6bb9ee8a7d0b4e0ff531b8921442d8
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-10 01:57:26 -0400

    (ryzen)edit:PdfViewCV(logica de renderizado CV-PDF al 95%)

--- frontend/src/components/pdfViewCV/PdfViewCV.tsx
+++ frontend/src/components/pdfViewCV/PdfViewCV.tsx
@@ -51,23 +51,32 @@ const PdfViewCV: React.FC = () => {
   };
 
 */
-  return (
-    <>
-      <div>
-        <div>
-          <button
-            onClick={() => downloaderFileAtURL(PDF_FILE_URL)}
-            className="gap-2 px-[20px] py-[5px] rounded mt-6 bg-[#1484d6] border border-[#0e0e0f] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6] "
-          >
-            Descarga mi CV
-          </button>
-        </div>
-        <div>
-        <iframe src="https://udeconce-my.sharepoint.com/personal/rigomartinez_udec_cl/_layouts/15/embed.aspx?UniqueId=b0fb60f4-bcba-472b-bafe-041bab3acce7" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="Rigoberto-CV-develop_C.pdf"></iframe>
-        </div>
+return (
+  <>
+    <div className="bg-stone-100 h-screen">
+      <div className="flex gap-10 justify-center mt-[0rem] mb-7">
+        <button
+          onClick={() => downloaderFileAtURL(PDF_FILE_URL)}
+          className="gap-2 px-[20px] py-[5px] rounded mt-6 bg-[#1484d6] border border-[#f5dabe] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6] "
+        >
+          Descarga mi CV
+        </button>
       </div>
-    </>
-  );
+      <div className="relative h-[80vh]">
+        <iframe className="absolute top-0 left-0 w-full h-full border-0" src="https://udeconce-my.sharepoint.com/personal/rigomartinez_udec_cl/_layouts/15/embed.aspx?UniqueId=b0fb60f4-bcba-472b-bafe-041bab3acce7" frameborder="0" scrolling="no" allowfullscreen title="Rigoberto-CV-develop_C.pdf"></iframe>
+      </div>
+      <div className="flex gap-10 justify-center mt-[0rem] mb-7">
+        <button
+          onClick={() => downloaderFileAtURL(PDF_FILE_URL)}
+          className="gap-2 px-[20px] py-[5px] rounded mt-6 bg-[#1484d6] border border-[#f5dabe] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6] "
+        >
+          Descarga mi CV
+        </button>
+      </div>
+    </div>
+  </>
+);
+
 };
 
 export default PdfViewCV;
