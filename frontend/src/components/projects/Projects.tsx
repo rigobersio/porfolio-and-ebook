2bfe36280b806a9ba05e19464b7b01e4d14dae1c
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-12 00:50:26 -0400

    (ryzen)edit:Project(esqueleto, animación y posicion)

--- frontend/src/components/projects/Projects.tsx
+++ frontend/src/components/projects/Projects.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Projects: React.FC = () => {
 
-    return (
-        <div id="Proyectos">
-            <h1>Acá mis proyectos</h1>
-        </div>
+  const [isProjectsVisible, setIsProjectsVisible] = useState(false);
+  
+  const { ref: projectsRef, inView: isProjectsInView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  })
 
-    );
+  useEffect(() => {
+    setIsProjectsVisible(isProjectsInView);
+  }, [isProjectsInView]);
+
+  return (
+    <div id="Proyectos" className="w-80 flex content-center direction-col" ref={projectsRef}>
+      <h2 className="text-center p-[3rem] text-3xl my-2 hover:text-purple-800 transition"><strong>Mis Proyectos</strong></h2>
+      <div className="bg-slate-800">
+      {isProjectsVisible && (
+          <p className="mx-auto">El proyecto se desarrolló en equipo utilizando una metodología de trabajo scrum para desarrollar una aplicación
+          web para alquiler de habitaciones de hoteles. Trabajamos de forma asíncrona en un contexto remoto e
+          internacional, colaborando en el manejo de tareas y gitflow para evitar conflictos en el código. Me asignaron
+          responsabilidades tanto en el desarrollo del backend como en la creación de componentes frontend.</p>
+        )}
+      </div>
+    </div>
+
+  );
 };
 
 export default Projects;
