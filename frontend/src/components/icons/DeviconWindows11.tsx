8382c0b06fa998e3d130447d09ea01306f4703fe
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-16 00:59:02 -0400

    (ryzen)enew:folderIcons(componentes iconos de icones.js.org)

---
+++ frontend/src/components/icons/DeviconWindows11.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function DeviconWindows11(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 128 128" {...props}><path fill="#0078d4" d="M67.328 67.331h60.669V128H67.328zm-67.325 0h60.669V128H.003zM67.328 0h60.669v60.669H67.328zM.003 0h60.669v60.669H.003z"></path></svg>
+  )
+}
+export default DeviconWindows11
