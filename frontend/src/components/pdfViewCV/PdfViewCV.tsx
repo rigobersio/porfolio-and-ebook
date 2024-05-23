e425cc3880106936b253033395e549db3136d91f
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-23 19:21:06 -0400

    (ryzen)edit:PdfVewCV(cambio de estetica resp)

--- frontend/src/components/pdfViewCV/PdfViewCV.tsx
+++ frontend/src/components/pdfViewCV/PdfViewCV.tsx
@@ -37,13 +37,13 @@ return (
   <>
     <div className="bg-orange-50 h-screen">
       
-      <div className="flex justify-center items-center h-[90%]">
+      <div className="flex justify-center items-center h-[85%] lg:h-[93%]">
         <iframe className="w-full h-full border-0" src={CloudCVPDF} frameBorder="0" scrolling="no" allowFullScreen title="Rigoberto-CV-develop_C.pdf"></iframe>
       </div>
-      <div className="flex gap-10 justify-center mt-[0rem] mb-2">
+      <div className="flex justify-center ">
         <button
           onClick={() => downloaderFileAtURL(PDF_FILE_URL)}
-          className="gap-2 px-[20px] py-[5px] rounded mt-6 bg-[#1484d6] border border-[#f5dabe] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6] "
+          className="lg:mt-5 mb-0 mt-2 px-[20px] py-[5px] rounded  bg-[#1484d6] border border-[#f5dabe] transition-all duration-700 hover:bg-transparent hover:text-[#1484d6] "
         >
           Descarga mi CV
         </button>
