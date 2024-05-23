aac1f01232e80dc57741b784ad02788fd9740929
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-23 16:24:36 -0400

    (ryzen)enew:vercel.json;edit:vite.config.ts(rutas para vercel, eliminacion base para pages)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://porfolio-rigoberto.vercel.app/',
+  //base: 'https://porfolio-rigoberto.vercel.app/',
 })
