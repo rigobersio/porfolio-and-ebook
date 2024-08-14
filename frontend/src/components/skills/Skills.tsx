6c2e272ffeaca6763b05efc126f7da8421e0bfcd
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-14 11:53:30 -0400

    (ri)edit:Skills(iconoMongo w12 h12)

--- frontend/src/components/skills/Skills.tsx
+++ frontend/src/components/skills/Skills.tsx
@@ -44,7 +44,7 @@ const Skills: React.FC = () => {
           <IconNode />
           <IconExpress />
           <IconPostgre />
-          <img src={IconMongoDB} alt="MongoDB logo" className="w-13 h-13"/>
+          <img src={IconMongoDB} alt="MongoDB logo" className="w-12 h-12"/>
           <IconGit />
           <IconGitHub />
           <IconWindows />
